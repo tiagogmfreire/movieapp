a7329564943a678d47236a200750c3bb432ceeb1
Tiago Galvão M. Freire <tiagogmfreire@gmail.com>
2019-06-06 10:37:37 -0300

    using cors in axios request

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,4 +12,22 @@
 export default {
   name: 'PageIndex'
 }
+
+import axios from 'axios'
+
+axios('http://localhost/movie/upcoming', {
+  method: 'GET',
+  mode: 'no-cors',
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json'
+  }
+})
+  .then(function (response) {
+    console.log(response)
+  })
+  .catch(function (response) {
+    console.log(response)
+  })
+
 </script>
