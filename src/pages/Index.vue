6de0bd8029002d658946b92eb38462a0f7f80c9c
Tiago Galvão M. Freire <tiagogmfreire@gmail.com>
2019-06-06 16:01:25 -0300

    Adjusting v-for to remove warnings

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,13 @@
 <template>
   <q-page class="flex flex-center">
     <div class="q-pa-md row items-start q-gutter-md">
-      <q-card class="my-card" v-for="movie in movies" v-bind:key="movie">
+      <q-card class="my-card" v-for="(movie, i) in movies" v-bind:key="`${i}-${movie}`">
         <img v-bind:src="movie.poster_path"  >
 
         <q-card-section>
           <div class="text-h6">{{movie.title}}</div>
           <div class="text-subtitle2">{{movie.release_date}}</div>
-          <div class="text-subtitle3" v-for="genre in movie.genres" v-bind:key="genre">{{genre.name}}</div>
+          <div class="text-subtitle3" v-for="(genre,j) in movie.genres" v-bind:key="`${j}-${genre}`">{{genre.name}}</div>
         </q-card-section>
 
         <q-card-actions>
