70c26bb8e99e9bdbcd4f1dc249ef803f2bc66030
Tiago Galvão M. Freire <tiagogmfreire@gmail.com>
2019-06-06 11:41:17 -0300

    Testing the endpoints of the api

--- src/pages/Movie.vue
+++ src/pages/Movie.vue
@@ -12,4 +12,21 @@
 export default {
   name: 'PageIndex'
 }
+
+import axios from 'axios'
+
+axios('http://localhost/movie/299534', {
+  method: 'GET',
+  mode: 'no-cors',
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json'
+  }
+})
+  .then(function (response) {
+    console.log(response)
+  })
+  .catch(function (response) {
+    console.log(response)
+  })
 </script>
